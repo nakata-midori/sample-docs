b4165331258aa3c903aa5e32f065fc937eef746c
Yu Nakata <yu.nakata@tier4.jp>
2025-06-21 15:49:03 +0900

    chore: 自分のアカウントが使われないようにした

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/sample-docs/', // リポジトリ名で終わるように
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
